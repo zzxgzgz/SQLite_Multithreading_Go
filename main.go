5eebf73f8b5b9ab09981b37456c72349983be2d1
Rio Zhu <zzhu@futurewei.com>
2022-03-16 14:41:05 -0700

    Modified code accordig to advice gained from mattn/sqlite3

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ var (
 	// MaxQueue Max Size of the Job Queue
 	MaxQueue = 1024
 	// NumberOfPeople how many people to generate
-	NumberOfPeople = 1024 * 800
+	NumberOfPeople = 1024 * 1024 * 20
 	// JobQueue a queue that sends the sql.Stmt jobs to the workers
 	JobQueue chan worker_pool.Job
 	letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
@@ -28,16 +28,30 @@ var (
 	allQueryStart time.Time
 	allQueryEnd time.Time
 	//database *sql.DB
-	db_connections []*sql.DB
+	//db_connections []*sql.DB
+	db_write_connection *sql.DB
+	db_create_connection *sql.DB
+	db_read_connection *sql.DB
 )
 
 func main(){
 	JobQueue = make(chan worker_pool.Job, MaxQueue)
-	db_connections = make([]*sql.DB, MaxWorker)
+	//db_connections = make([]*sql.DB, MaxWorker)
 	log.Println("Hello world!")
-	for i := 0 ; i < MaxWorker ; i ++ {
-		db_connections[i], _ = sql.Open("sqlite3", "./rio_testing.db?cache=shared&mode=rwc")
-	}
+	//for i := 0 ; i < MaxWorker ; i ++ {
+	//	db_connections[i], _ = sql.Open("sqlite3", "file:./rio_testing.db?cache=shared&mode=rwc")
+	//}
+	// mode: read write
+	db_write_connection, _ = sql.Open("sqlite3", "file:./rio_testing.db?&mode=rw&_journal_mode=wal")
+	// only ONE writer, as SQLite doesn't support multiple concurrent write.
+	db_write_connection.SetMaxOpenConns(1)
+	// mode: read only
+	db_read_connection, _ = sql.Open("sqlite3", "file:./rio_testing.db?&mode=ro&_journal_mode=wal")
+	// can have many readers
+	db_read_connection.SetMaxOpenConns(MaxWorker)
+	// mode: read write create
+	db_create_connection, _ = sql.Open("sqlite3", "file:./rio_testing.db?&mode=rwc&_journal_mode=wal")
+
 	// Open the database, this command creates the .db file if it doesn't exist.
 	//database, _ = sql.Open("sqlite3", "./rio_testing.db?cache=shared&mode=rwc")
 	//database.SetMaxOpenConns(MaxWorker)
@@ -47,18 +61,21 @@ func main(){
 	dispatcher.Run()
 
 	// Create table if it doesn't already exist/
-	CreateTableStatement, _ := db_connections[0].Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
+	CreateTableStatement, _ := db_create_connection.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
 	CreateTableStatement.Exec()
 
 	// Clear the table so that it is empty.
-	TruncateTableStatement, _ := db_connections[0].Prepare("DELETE FROM people")
+	TruncateTableStatement, _ := db_create_connection.Prepare("DELETE FROM people")
 	TruncateTableStatement.Exec()
 
-	CreateFirstNameIndexStatement, _ := db_connections[0].Prepare("CREATE INDEX idx_people_firstname ON people (firstname)")
+	CreateFirstNameIndexStatement, _ := db_create_connection.Prepare("CREATE INDEX idx_people_firstname ON people (firstname)")
 	CreateFirstNameIndexStatement.Exec()
 
-	CreateLastNameIndexStatement, _ := db_connections[0].Prepare("CREATE INDEX idx_people_lastname ON people (lastname)")
+	CreateLastNameIndexStatement, _ := db_create_connection.Prepare("CREATE INDEX idx_people_lastname ON people (lastname)")
 	CreateLastNameIndexStatement.Exec()
+
+	// close the create table connection
+	db_create_connection.Close()
 
 	GeneratePeople()
 
@@ -66,6 +83,9 @@ func main(){
 	InsertPeopleIntoDB()
 	insertPeopleEndTime := time.Now()
 	log.Printf("Inserting %d people took %v", NumberOfPeople, insertPeopleEndTime.Sub(insertPeopleStartTime))
+
+	// close the write connection
+	db_write_connection.Close()
 
 	wg.Add(NumberOfPeople)
 	QueryPeopleFromDB()
@@ -107,7 +127,7 @@ func GeneratePeople(){
 // InsertPeopleIntoDB When writing into a sqlite db, we'd better do it sequentially,
 // or it will cause LOCK related errors
 func InsertPeopleIntoDB() {
-	tx, _ := db_connections[0].Begin()
+	tx, _ := db_write_connection.Begin()
 	InsertStatement, _ = tx.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
 	for i, _ := range peopleSlice {
 		ArgumentsInterface := make([]interface{}, 2)
@@ -138,14 +158,16 @@ func QueryPeopleFromDB(){
 	//	}
 	//}
 	allQueryStart = time.Now()
-	QueryStatementSlice := make([]*sql.Stmt, MaxWorker)
-	for i := 0 ; i < MaxWorker ; i ++ {
-		QueryStatementSlice[i], _ = db_connections[i].Prepare("SELECT id, firstname, lastname FROM people WHERE firstname = ? AND lastname = ?")
-	}
+	//QueryStatementSlice := make([]*sql.Stmt, MaxWorker)
+	//for i := 0 ; i < MaxWorker ; i ++ {
+	//	QueryStatementSlice[i], _ = db_connections[i].Prepare("SELECT id, firstname, lastname FROM people WHERE firstname = ? AND lastname = ?")
+	//}
+
+	QueryStatement, _ = db_read_connection.Prepare("SELECT id, firstname, lastname FROM people WHERE firstname = ? AND lastname = ?")
 	//QueryStatement, _ = database.Prepare("SELECT id, firstname, lastname FROM people WHERE firstname = ? AND lastname = ?")
 	for index, _ := range peopleSlice {
 		JobQueue <- worker_pool.Job{
-			Payload: QueryStatementSlice[index % MaxWorker],
+			Payload: QueryStatement,
 			Args:    []interface{}{peopleSlice[index].FirstName, peopleSlice[index].LastName},
 		}
 	}
