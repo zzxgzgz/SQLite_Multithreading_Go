be3f378403df2add1e3504a0123c19f6280d6d6c
Rio Zhu <zzhu@futurewei.com>
2022-04-05 16:03:17 -0700

    Changed DB schema to Virtual IP and Host IP

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module SQLite_Multithreading_Go
 
 go 1.14
 
-require github.com/mattn/go-sqlite3 v1.14.12
+require (
+	github.com/3th1nk/cidr v0.0.0-20210818063431-caa3bf6ce590
+	github.com/mattn/go-sqlite3 v1.14.12
+)
--- main.go
+++ main.go
@@ -1,45 +1,68 @@
 package main
 
-
-
 import (
 	"database/sql"
 	"fmt"
+	"github.com/3th1nk/cidr"
+	_ "github.com/mattn/go-sqlite3"
 	"math"
+	"math/big"
+	"net"
 	"os"
 	"runtime"
+	"strconv"
 	"sync"
 	"time"
-	_ "github.com/mattn/go-sqlite3"
 )
 
 var stub = make([]byte, 1024 * 1024 * 1024 * 4)
 
 func main(){
+	destination_host_ip := "123.223.32.4"
+	destination_host_ip_int := int(IP6toInt(net.ParseIP(destination_host_ip)).Int64())
 	// allow the program to use all cores.
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	// allocate memory for the program.
 	stub[0] = 1
-	type BCCode struct {
-		B_Code string
-		C_Code string
-		CodeType int
-		IsNew int
+	type Row struct {
+		RowKey int64
+		RowValue int64
 	}
+
+	vni_slice := make([]int, 0)
+
+	// add a default VNI to this slice, could add more in the future.
+	vni_slice = append(vni_slice, 1)
+
+	// Create a CIDR for all VPCs
+	cidr_pointer, _ := cidr.ParseCIDR("10.0.0.0/24")
+
+	ip_slice := make([]int, 0)
+
+	if cidr_pointer.IsIPv4(){
+		cidr_pointer.ForEachIP(func(ip string) error {
+			ip_slice = append(ip_slice, int(IP6toInt(net.ParseIP(ip)).Int64()))
+			return nil
+		})
+	}else if cidr_pointer.IsIPv6(){
+		// TODO: Implement IPV6 function
+	}else{
+		panic("This CIDR is neither v4 nor v6, existing")
+	}
+
+
+
+	fmt.Printf("There are %v IPs in this CIDR %s\n", len(ip_slice), cidr_pointer.CIDR())
+
 	number_of_go_routines := 20
 
 	// Open the connection to the DB, will create the .db file is it does not exist.
 	db, err := sql.Open("sqlite3", "file:./rio_testing.db?loc=auto&_journal_mode=wal&_mutex=no")
 
-
-
 	// one DB connection can be used by two go-routines
 	number_of_db_connections := int(math.Ceil(float64(number_of_go_routines) / 2.0))
 
-
-
 	db_connections := make([]*sql.DB, number_of_db_connections)
-
 
 	// create multiple db connections for concurrent read.
 	for i := 0 ; i < number_of_db_connections ; i ++{
@@ -51,15 +74,15 @@ func main(){
 		fmt.Println("SQLite:", err)
 	}
 	fmt.Println("SQLite start")
-	//Create the table
-	sqlStmt := `create table BC (b_code text not null primary key, c_code text not null, code_type INTEGER, is_new INTEGER);`
+	//Create the table, INTEGER key and INTEGER value
+	sqlStmt := `create table IF NOT EXISTS BC (RowKey INTEGER not null primary key, RowValue INTEGER not null);`
 	_, err = db.Exec(sqlStmt)
 	if err != nil {
 		fmt.Printf("create table error->%q: %s\n", err, sqlStmt)
 		return
 	}
 	//Create index on column
-	_, err = db.Exec("CREATE INDEX inx_c_code ON BC(c_code);")
+	_, err = db.Exec("CREATE INDEX index_row_key ON BC(RowKey);")
 	if err != nil {
 		fmt.Println("create index error->%q: %s\n", err, sqlStmt)
 		return
@@ -71,18 +94,18 @@ func main(){
 		fmt.Printf("%q\n", err)
 	}
 	// prepare insert statement
-	stmt, err := tx.Prepare("insert into BC(b_code, c_code, code_type, is_new ) values(?,?,?,?)")
+	stmt, err := tx.Prepare("insert into BC(RowKey, RowValue) values(?,?)")
 	if err != nil {
 		fmt.Printf("insert err %q\n", err)
 	}
 	db.Close()
 	defer stmt.Close()
-	var m int = 1000 * 1000
+	var m int = len(ip_slice)//1000 * 1000
 	var total int = 1 * m
 	for i := 0; i < total; i++ {
-		_, err = stmt.Exec(fmt.Sprintf("B%024d", i), fmt.Sprintf("C%024d", i), 0, 1)
+		_, err = stmt.Exec(ip_slice[i], destination_host_ip_int)
 		if err != nil {
-			fmt.Println("%q", err)
+			fmt.Printf("Insert error: %q", err)
 		}
 	}
 	tx.Commit()
@@ -92,7 +115,7 @@ func main(){
 	// Prepare the select statements for each db read connection.
 	query_statements := make([]*sql.Stmt, number_of_db_connections)
 	for i := 0 ; i < number_of_db_connections ; i ++{
-		query_statements[i], _ = db_connections[i].Prepare("select b_code from BC where c_code = ? ")
+		query_statements[i], _ = db_connections[i].Prepare("select RowValue from BC where RowKey = ? ")
 	}
 	// Start querying in each go-routine, each go-routine query `total` times.
 	for i := 0 ; i < number_of_go_routines ; i ++ {
@@ -108,13 +131,11 @@ func main(){
 			var b string
 			for j := 0; j < total; j++ {
 				//var rowCount int
-				err := query_statement.QueryRow(fmt.Sprintf("C%024d", j)).Scan(&b)
+				err := query_statement.QueryRow(ip_slice[i]).Scan(&b)
 				if err != nil {
 					fmt.Printf("query err %q", err)
 					os.Exit(-1)
 				}
-
-
 
 				// Comment out the outputs, as it slows down the code.
 				//fmt.Println("BCode=", bc.B_Code, "\tCCode=", bc.C_Code, "\tCodeType=", bc.CodeType, "\tIsNew=", bc.IsNew)
@@ -129,4 +150,38 @@ func main(){
 	}
 	wg.Wait()
 	fmt.Println("All finished.")
+}
+
+//Copied from https://go.dev/play/p/JVJuTsxXR-
+func padding(n int32) int32 {
+	var p int32 = 1
+	for p < n {
+		p *= 10
+	}
+	return p
+}
+
+//func ip2Long(ip string) uint32 {
+//	var long uint32
+//	binary.Read(bytes.NewBuffer(net.ParseIP(ip).To4()), binary.BigEndian, &long)
+//	return long
+//}
+
+
+func IP6toInt(IPv6Address net.IP) *big.Int {
+	IPv6Int := big.NewInt(0)
+
+	// from http://golang.org/pkg/net/#pkg-constants
+	// IPv6len = 16
+	IPv6Int.SetBytes(IPv6Address.To16())
+	return IPv6Int
+}
+
+func backtoIP4(ipInt int64) string {
+	// need to do two bit shifting and “0xff” masking
+	b0 := strconv.FormatInt((ipInt>>24)&0xff, 10)
+	b1 := strconv.FormatInt((ipInt>>16)&0xff, 10)
+	b2 := strconv.FormatInt((ipInt>>8)&0xff, 10)
+	b3 := strconv.FormatInt((ipInt & 0xff), 10)
+	return b0 + "." + b1 + "." + b2 + "." + b3
 }
